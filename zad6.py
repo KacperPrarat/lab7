1f90f509e3a2962c3207e039a202d0033bc4dea9
KacperPrarat <115100470+KacperPrarat@users.noreply.github.com>
2022-11-29 23:21:10 +0100

    Update zad6.py

--- zad6.py
+++ zad6.py
@@ -1,8 +1,5 @@
-
-
 dict1 = {1: 10, 2: 7, 3: 8, 4: 15, 5: 14, 6: 3, 7: 2}
-dict2 = {8:10,9:40,10:34,11:100}
-
+dict2 = {8: 10, 9: 40, 10: 34, 11: 100}
 def find_max(dict):
     max=0
     for number in dict:
@@ -15,18 +12,25 @@ def merge_dictionaries(dict1,dict2):
     dict3.update(dict2)
     return dict3
 
+
+def main():
+    dict1 = {1: 10, 2: 7, 3: 8, 4: 15, 5: 14, 6: 3, 7: 2}
+    dict2 = {8: 10, 9: 40, 10: 34, 11: 100}
+    dict3 = merge_dictionaries(dict1,dict2)
+    print("pierwszy",dict1,"drugi",dict2,"trzeci",dict3)
+
 def odd_sum_dictionaries(dict):
-    licznik = 0
-    argument = 1
-    while argument <= len(dict):
-        licznik=licznik+dict[argument]
-        argument = argument+2
-    return licznik
+    nrwart = 1
+    suma = 0
+
+    while nrwart <= len(dict) :
+        suma = suma + dict[nrwart]
+        nrwart = nrwart + 2
+    return suma 
 
 print(odd_sum_dictionaries(dict1))
 
 
 
-
-
-
+if __name__ == "__main__":
+    main()
