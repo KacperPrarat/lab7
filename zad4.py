64152a66ec5a6b561b903f5c24b1379f835687d6
KacperPrarat <115100470+KacperPrarat@users.noreply.github.com>
2022-11-30 12:46:00 +0100

    Update zad4.py

--- zad4.py
+++ zad4.py
@@ -1,11 +1,9 @@
-def find_max(dict):
-    max=0
-    for number in dict:
-        if number > max:
-            max = number
-    return max
+dict1= {1:10,2:7,3:8,4:15,5:14,6:3,7:2}
+def find_max(dict1):
+    naj = 0
+    for klucz in dict1:
+        if dict1[klucz] > naj:
+            naj = dict1[klucz]
+    return naj
 
-
-
-dict1 = {1:10,2:7,3:8,4:15,5:14,6:3,7:2}
 print(find_max(dict1))
