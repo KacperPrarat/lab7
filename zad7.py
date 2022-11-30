f88006455147e5ba06ef8f00d2edba18c357be18
KacperPrarat <115100470+KacperPrarat@users.noreply.github.com>
2022-11-30 12:15:34 +0100

    Update zad7.py

--- zad7.py
+++ zad7.py
@@ -2,12 +2,15 @@ dicta = {1: 10,2: 20,3: 30,4: 40}
 dictb = {1:11,3:33,5:55}
 def sum_dictionaries(dict1,dict2):
     newdict={}
-    for i in dict1:
-
+    for i in range(list(dict1.keys())[-1]):
+        if not i in dict1.keys():
+            continue
+        if not i in dict2.keys():
+            continue
         x = (dict1[i])
         z = (dict2[i])
         wynik = x + z
-        newdict[i] = wynik
+        newdict.update([[i, wynik]])
     return newdict
 
 
